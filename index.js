0ab6f7194276f6d2e4986f2f1e46f9c25fc9b7b0
Your Name <yasminaliabdi601@gmail.com>
2026-01-21 03:44:04 -0800

    done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+function findMatching(drivers, name) {
+  return drivers.filter(driver => 
+    driver.toLowerCase() === name.toLowerCase()
+  );
+}
+
+function fuzzyMatch(drivers, letters) {
+  return drivers.filter(driver =>
+    driver.toLowerCase().startsWith(letters.toLowerCase())
+  );
+}
+
+function matchName(drivers, name) {
+  return drivers.filter(driver =>
+    driver.name === name
+  );
+}
